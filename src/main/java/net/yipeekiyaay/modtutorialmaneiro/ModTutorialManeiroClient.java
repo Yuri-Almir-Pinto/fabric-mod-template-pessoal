51b89a03c5cea98e1af84666ce8eada2eaa04baa
Yuri-Almir-Pinto <yurialmirp@gmail.com>
2023-10-07 00:15:11 -0300

    Commit do template

---
+++ src/main/java/net/yipeekiyaay/modtutorialmaneiro/ModTutorialManeiroClient.java
@@ -0,0 +1,10 @@
+package net.yipeekiyaay.modtutorialmaneiro;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTutorialManeiroClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/yipeekiyaay/modtutorialmaneiro/ModTutorialManeiroDataGenerator.java
@@ -0,0 +1,11 @@
+package net.yipeekiyaay.modtutorialmaneiro;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ModTutorialManeiroDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
